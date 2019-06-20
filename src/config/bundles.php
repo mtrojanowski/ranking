4969f4b6766d9cb6eed8e6f07116968fa1d42b99
Michal Trojanowski <michal.trojanowski@allegro.pl>
2019-06-20 18:41:06 +0200

    Configure monolog

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
