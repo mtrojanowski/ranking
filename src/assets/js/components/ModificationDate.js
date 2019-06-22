a46615ad7c87062366c6cbe36ee92ca89265f4e7
Michal Trojanowski <michal.trojanowski@allegro.pl>
2019-06-22 12:28:21 +0200

    Proper ranking title

--- src/assets/js/components/ModificationDate.js
+++ src/assets/js/components/ModificationDate.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class ModificationDate extends Component {
     render() {
         const { lastModified } = this.props;
-        const showModificationDate = lastModified !== undefined && lastModified !== null;
+        const showModificationDate = lastModified !== undefined && lastModified !== null && lastModified !== "";
         return (<div className="modification-date">
             {showModificationDate && <div>
                 ostatnio zmodyfikowany: { lastModified }
